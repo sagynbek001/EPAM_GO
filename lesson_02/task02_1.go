0acfa9eb67b5470dd75e21c3379ae097bd6df17c
Sagynbek Talgatuly <81487954+sagynbek001@users.noreply.github.com>
2021-07-10 03:21:40 +0500

    Update task02_1.go

--- lesson_02/task02_1.go
+++ lesson_02/task02_1.go
@@ -10,7 +10,7 @@ func main() {
 	defer fmt.Println("Thank you for running this program!")
 	fmt.Println("Hello, this application prints first 10 Fibonacci numbers!")
 	fmt.Println("Using a loop:")
-	fib.Print_fib_loop(10)
+	fib.PrintFibLoop(10)
 	fmt.Println("Using recursion:")
-	fib.Print_fib_recursion(10)
+	fib.PrintFibRecursion(10)
 }
